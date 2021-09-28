b294d962850a790b2377a612ca88655f4f7ac1d6
Stepan Kirushkin <stepan.kirushkin@gmail.com>
2021-09-28 11:29:21 +0300

    Better Rails middleware init.

--- lib/memolog/railtie.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Memolog::Railtie < Rails::Railtie
-  initializer "memolog.configure_rails_initialization" do |app|
-    app.middleware.insert_before(0, Memolog::Middleware)
-  end
-end
