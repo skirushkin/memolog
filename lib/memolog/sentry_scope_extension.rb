6dd8b7cb22f1bc60218acb6a8ea351939393253e
Stepan Kirushkin <stepan.kirushkin@gmail.com>
2021-11-20 21:20:18 +0300

    Small fix for method definition.

--- lib/memolog/sentry_scope_extension.rb
+++ lib/memolog/sentry_scope_extension.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Memolog::SentryScopeExtension
-  def apply_to_event(scope, hint)
+  def apply_to_event(scope, hint = nil)
     memolog_dump = Memolog.dump
     set_extras(Memolog.config.sentry_key => memolog_dump) if memolog_dump.present?
 
